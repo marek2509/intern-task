1a07ae9fb17622faa646405554b71f52dd4ac640
Marek <marek.wojciechowicz25@gmail.com>
2022-05-31 00:25:46 +0200

    Finished intern-task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import './App.css';
-import Dot from "./Dot";
+import Dot from './Dot';
 
 function App() {
-    return (
-        <div className="dots-wrapper">
-            {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i}/>
-            ))}
-        </div>
-    );
+	const [dotNumber, setDotNumber] = useState(false);
+	const [dotActive, setDotActive] = useState(false);
+
+	const handleDotActiveChange = (dotId) => {
+		if (dotNumber === dotId) {
+			setDotActive(false);
+		}
+
+		setDotNumber(dotId);
+	};
+
+	return (
+		<div className="dots-wrapper">
+			{new Array(20).fill('#000000').map((_, i) => (
+				<Dot
+					key={i}
+					onClick={() => handleDotActiveChange(i)}
+					isActive={dotNumber === i}
+				>
+					{i + 1}
+				</Dot>
+			))}
+		</div>
+	);
 }
 
 export default App;
--- src/Dot.js
+++ src/Dot.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import './Dot.css';
 
-const Dot = () => {
-    // TODO: put your code here
-    return (
-        <div className="dot"/>
-    );
+const Dot = ({ children, onClick, isActive }) => {
+	const [activeBefore, setActiveBefore] = useState(false);
+
+	const handleDotActiveBefore = () => {
+		if (activeBefore && isActive) {
+			setActiveBefore(false);
+		} else {
+			setActiveBefore(isActive);
+		}
+	};
+
+	return (
+		<div
+			className={
+				isActive && isActive !== activeBefore ? 'dot activeDot' : 'dot'
+			}
+			onClick={() => {
+				handleDotActiveBefore();
+				onClick();
+			}}
+		>
+			<p>{children}</p>
+		</div>
+	);
 };
 
 export default Dot;
